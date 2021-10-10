844281654c9a18e227e2270653bd1909bb7737f2
tuantm71 <tuantm71@fpt.com.vn>
2021-10-10 15:20:10 +0700

    create crud product

--- src/services/sub-category.js
+++ src/services/sub-category.js
@@ -21,7 +21,7 @@ export async function getSubCategory(opts = {}) {
     }
   }
   
-  export async function getSubCategory(opts = {}) {
+  export async function getAllSubCategory(opts = {}) {
     const queryObj = {...opts}
   
     try {

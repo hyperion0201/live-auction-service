ce97b19422cea26b79afc6c32a63a96ca29216f3
Phuong <daoanhphuong080699@gmail.com>
2021-10-15 20:25:33 +0700

    update router category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -7,14 +7,62 @@ const router = express.Router();
 
 router.get("/", authenticate(), async (req, res, next) => {
   try {
-    const biddingRecords = await categoryService.getCategories();
-    res.json(biddingRecords);
+    const categories = await categoryService.getCategories();
+    res.json(categories);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", authenticate(), async (req, res, next) => {
+  const id = +req.params.id;
+
+  try {
+    const category = await categoryService.getByID({ _id: id });
+    if (!category) {
+      return res
+        .status(HTTP_STATUS_CODES.NOT_FOUND)
+        .json({ message: "find not found" });
+    }
+    res.json(category);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const payload = req.body;
+  try {
+    const newCategory = await categoryService.addNew(payload);
+    res.json(newCategory);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch("/:id", authenticate(), async (req, res, next) => {
+  const id = +req.params.id;
+  const payload = req.body;
+  try {
+    await categoryService.updateOneCategory({ _id: id }, payload);
+    res.json({ message: "Update category successfully" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", authenticate(), async (req, res, next) => {
+  const id = +req.params.id;
+
+  try {
+    await categoryService.deleteOneCategory({ _id: id });
+    res.json({ message: "Delete success" });
   } catch (err) {
     next(err);
   }
 });
 
 export default {
-  prefix: "/v1/category",
+  prefix: "/v1/categories",
   routerInstance: router,
 };

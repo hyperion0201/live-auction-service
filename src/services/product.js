2bda7de24bdc4c1ab09bda99ed87dee108bc6c41
Phuong <daoanhphuong080699@gmail.com>
2021-10-15 20:56:16 +0700

    fix lint

--- src/services/product.js
+++ src/services/product.js
@@ -1,55 +1,55 @@
 import Product from '../models/product.js'
-import ServerError from '..utils/custom-error.js'
+import ServerError from '../utils/custom-error'
 
 export async function createProduct(payload = {}, opts = {}) {
-    try {
-        return await Product.create({ ...payload })
-    }
-    catch (err) {
-        throw new ServerError({ name: 'Something error when create bidding product.', err })
-    }
+  try {
+    return await Product.create({...payload})
+  }
+  catch (err) {
+    throw new ServerError({name: 'Something error when create bidding product.', err})
+  }
 }
 
 export async function getProduct(opts = {}) {
-    const queryObj = {...opts}
+  const queryObj = {...opts}
   
-    try {
-      return await Product.findOne(queryObj)
-    }
-    catch (err) {
-      throw new ServerError({name: 'Something error when get detail bidding product.', err})
-    }
+  try {
+    return await Product.findOne(queryObj)
   }
-  
-  export async function getAllProduct(opts = {}) {
-    const queryObj = {...opts}
-  
-    try {
-      return await Product.find(queryObj)
-    }
-    catch (err) {
-      throw new ServerError({name: 'Something error when get all bidding product.', err})
-    }
+  catch (err) {
+    throw new ServerError({name: 'Something error when get detail bidding product.', err})
   }
+}
   
-  export async function updateProduct(opts = {}, payload) {
-    const queryObj = {...opts}
+export async function getAllProduct(opts = {}) {
+  const queryObj = {...opts}
   
-    try {
-      return await Product.updateOne(queryObj, payload)
-    }
-    catch (err) {
-      throw new ServerError({name: 'Something error when update bidding product.', err})
-    }
+  try {
+    return await Product.find(queryObj)
   }
-  
-  export async function deleteProduct(opts = {}) {
-    const queryObj = {...opts}
-  
-    try {
-      return await Product.deleteOne(queryObj)
-    }
-    catch (err) {
-      throw new ServerError({name: 'Something error when delete bidding product.', err})
-    }
+  catch (err) {
+    throw new ServerError({name: 'Something error when get all bidding product.', err})
   }
+}
+  
+export async function updateProduct(opts = {}, payload) {
+  const queryObj = {...opts}
+  
+  try {
+    return await Product.updateOne(queryObj, payload)
+  }
+  catch (err) {
+    throw new ServerError({name: 'Something error when update bidding product.', err})
+  }
+}
+  
+export async function deleteProduct(opts = {}) {
+  const queryObj = {...opts}
+  
+  try {
+    return await Product.deleteOne(queryObj)
+  }
+  catch (err) {
+    throw new ServerError({name: 'Something error when delete bidding product.', err})
+  }
+}
